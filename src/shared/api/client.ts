1d1d26ddd772c8d758c8a928c316197014db6f37
Veronika Dmitrieva <veronika.dmitrieva.1990@mail.ru>
2026-09-11 11:25:02 +0300

    add Middleware

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -1,5 +1,15 @@
-import createClient from "openapi-fetch"
+import createClient, {type Middleware} from "openapi-fetch"
 import type { paths } from "./schema"
+
+const authMiddleware: Middleware = {
+    async onRequest({request}) {
+        const accessToken = localStorage.getItem("musicfun-access-token")
+        if(accessToken) {
+            request.headers.set("Authorization", "Bearer " + accessToken)
+        }
+        return request
+    }
+}
 
 export const client = createClient<paths>(
     { baseUrl: "https://musicfun.it-incubator.app/api/1.0/",
@@ -7,3 +17,5 @@ export const client = createClient<paths>(
             "api-key": import.meta.env.VITE_API_KEY,
         } }
 )
+
+client.use(authMiddleware)
