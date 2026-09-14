e703b0f73cb87674369624dbb4cb460b0539b9c3
Veronika Dmitrieva <veronika.dmitrieva.1990@mail.ru>
2026-09-14 15:37:42 +0300

    delete links

--- src/shared/ui/header/header.tsx
+++ src/shared/ui/header/header.tsx
@@ -10,10 +10,9 @@ export const Header = ({renderAccountBar}: Props) => {
     return (
         <header className={styles.header}>
             <div className={styles.container}>
+
                 <div className={styles.linksBlock}>
                     <Link to="/">Playlists</Link>
-                    <Link to="/my-playlists">My Playlists</Link>
-                    <Link to="/oauth/callback">temp page</Link>
                 </div>
 
                 <div>{renderAccountBar()}</div>
