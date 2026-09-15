3014c42a41a6676714b11aa3535251a621394261
Veronika Dmitrieva <veronika.dmitrieva.1990@mail.ru>
2026-09-15 18:00:51 +0300

    refetchType

--- src/features/playlists/add-playlist/ui/add-playlist-form.tsx
+++ src/features/playlists/add-playlist/ui/add-playlist-form.tsx
@@ -1,4 +1,4 @@
-import {useMutation} from "@tanstack/react-query"
+import {useMutation, useQueryClient} from "@tanstack/react-query"
 import {useForm} from "react-hook-form"
 import {client} from "../../../../shared/api/client"
 import type {SchemaCreatePlaylistData} from "../../../../shared/api/schema"
@@ -6,12 +6,19 @@ import type {SchemaCreatePlaylistData} from "../../../../shared/api/schema"
 export const AddPlaylistForm = () => {
     const {register, handleSubmit} = useForm<SchemaCreatePlaylistData>()
 
+    const queryClient = useQueryClient()
+
     const {mutate} = useMutation({
         mutationFn: async (data: SchemaCreatePlaylistData) => {
             const response = await client.POST("/playlists", {
                 body: {data}
             })
             return response.data
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ["playlists"],
+            })
         }
     })
 
