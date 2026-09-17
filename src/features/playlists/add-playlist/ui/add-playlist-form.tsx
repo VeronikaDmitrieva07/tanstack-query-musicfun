4b59425449113645e2d6f72f9e008682accf51df
Veronika Dmitrieva <veronika.dmitrieva.1990@mail.ru>
2026-09-17 19:45:00 +0300

    Close AddPlaylistForm, mutateAsync

--- src/features/playlists/add-playlist/ui/add-playlist-form.tsx
+++ src/features/playlists/add-playlist/ui/add-playlist-form.tsx
@@ -3,21 +3,29 @@ import type {SchemaCreatePlaylistData} from "../../../../shared/api/schema"
 import {useAddPlaylistMutation} from "../api/use-add-playlist-mutation"
 
 export const AddPlaylistForm = () => {
-    const {register, handleSubmit} = useForm<SchemaCreatePlaylistData>()
+    const {register, handleSubmit, reset} = useForm<SchemaCreatePlaylistData>()
 
-    const {mutate} = useAddPlaylistMutation()
+    const {mutateAsync} = useAddPlaylistMutation()
 
-    const onSubmit = (data: SchemaCreatePlaylistData) => {
-        mutate({...data, type: "playlists"})
+    const onSubmit = async (data: SchemaCreatePlaylistData) => {
+        try {
+            await mutateAsync({...data, type: "playlists"})
+        } catch {
+            console.log("error")
+        } finally {
+            reset()
+        }
     }
 
     return <form onSubmit={handleSubmit(onSubmit)}>
         <h2>Add New Playlist</h2>
 
-        <p> <input {...register("attributes.title")}/> </p>
+        <p><input {...register("attributes.title")}/></p>
 
-        <p> <textarea {...register("attributes.description")}/> </p>
+        <p><textarea {...register("attributes.description")}/></p>
 
-        <p> <button type={"submit"}>Create</button> </p>
+        <p>
+            <button type={"submit"}>Create</button>
+        </p>
     </form>
 }
